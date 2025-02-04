1805a42af0d339aed21ba589b3c5a5bda2ca7f7a
Mahfuz Mia <sayhello.mahfuz@gmail.com>
2025-02-05 02:57:11 +0600

    first release

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-
-
-echo "Building the project"
-python3 -m pip install -r requirements.txt
-
-
-echo "Migrating database"
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-
+ echo "BUILD START"
+ python3 -m pip install -r requirements.txt
+ echo "BUILD END"
